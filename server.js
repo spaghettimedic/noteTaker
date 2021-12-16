0fc0dfeed3fb6e287637f51c370acac932907a0f
spaghettimedic <dsteiner247@gmail.com>
2021-12-16 12:53:32 -0500

    implemented front-end with all functionality intact (except deletion of notes)

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ let counter = 0;
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// make assets readily available
+app.use(express.static('public'));
 
 const { notes } = require('./db/db');
 
@@ -62,6 +64,18 @@ app.get('/api/notes/:id', (req, res) => {
   } else {
     res.sendStatus(404);
   }
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/notes', (req, res) => {
