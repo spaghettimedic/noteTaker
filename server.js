530e44731e488a9fd44c98535a86d67929bef72c
spaghettimedic <dsteiner247@gmail.com>
2021-12-16 12:36:01 -0500

    allow user creation of data and validate data before allowing user to create it

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+let counter = 0;
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 const { notes } = require('./db/db');
 
 function filterByQuery(query, notesArray) {
@@ -14,6 +24,27 @@ function filterByQuery(query, notesArray) {
 function findById(id, notesArray) {
   const result = notesArray.filter(note => note.id === id)[0];
   return result;
+};
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+
+  return note;
+};
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!note.text || typeof note.text !== 'string') {
+    return false;
+  }
+  return true;
 };
 
 app.get('/api/notes', (req, res) => {
@@ -30,6 +61,20 @@ app.get('/api/notes/:id', (req, res) => {
     res.json(result);
   } else {
     res.sendStatus(404);
+  }
+});
+
+app.post('/api/notes', (req, res) => {
+  // set id based on what the index of the array will be
+  req.body.id = notes.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted.');
+  } else {
+    // add note to json file and notes array in this function
+    const note = createNewNote(req.body, notes);
+    res.json(note);
   }
 });
 
