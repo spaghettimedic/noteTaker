2e96a7d347c22fdcd90621ab6b3360ca0ef12a4b
spaghettimedic <dsteiner247@gmail.com>
2021-12-17 18:38:43 -0500

    added 'location.reload()' to delete cascade

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -92,6 +92,7 @@ const handleNoteDelete = (e) => {
   deleteNote(noteId).then(() => {
     getAndRenderNotes();
     renderActiveNote();
+    location.reload();
   });
 };
 
--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,11 +1,23 @@
-const { createNewNote, validateNote, deleteNote } = require('../../lib/notes');
+const { filterByQuery, findById, createNewNote, validateNote, deleteNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 const router = require('express').Router();
 const uniqid = require('uniqid');
 
 router.get('/notes', (req, res) => {
   let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
   res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 router.post('/notes', (req, res) => {
